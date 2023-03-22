114788476ca2f2563942335b2c21e7f912d6bf72
deadoutsideugly <>
2023-03-22 20:09:56 +0000

    arr2

--- .ccls-cache/@home@runner@SarcasticAgedDos/main.cpp
+++ .ccls-cache/@home@runner@SarcasticAgedDos/main.cpp
@@ -1,39 +1,34 @@
 #include <iostream>
 using namespace std;
 
-int func(double arr[], int n)
+void func(double arr[],int n)
 {
-  int x=0;
-  double av, sum=0;
-  for(int i = 0;i<n;i++)
+  int maxInd=0;
+  int minInd=0;
+  for(int i=0;i<n;i++)
     {
-      sum+=arr[i];
+      if(arr[i]>arr[maxInd])
+      {
+        maxInd=i;
+      }
+      else if (arr[i]<arr[minInd])
+      {
+        minInd=i;
+      }
     }
-  av=sum/n;
-   for(int i=0;i<n;i++)
-     {
-       if(arr[i]>av)
-       {
-         x++;
-       }
-     }
-  return x;
-}
-
-int main() {
-  int x=0;
-  const int n=9;
-  double arr[n];
+  arr[maxInd]=arr[minInd]=0;
   
-
-  for(int i =0;i<n;i++)
+}
+int main() {
+  const int n=16;
+  double arr[n];
+  for(int i=0;i<n;i++)
     {
       cin >> arr[i];
     }
-  
-  x=func(arr,n);
-    
-  cout<<x<<endl;
-
-  return 0;
+  func(arr,n);
+  for(int i=0;i<n;i++)
+    {
+      cout<<arr[i]<<"\t";
+    }
 }
