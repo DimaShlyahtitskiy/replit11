cb7e7de95f4f7320bf3ab16c74ed7327d71ef442
f1q1q1 <78561224+DimaShlyahtitskiy@users.noreply.github.com>
2023-03-22 20:10:51 +0000

    init commit

--- main.cpp
+++ main.cpp
@@ -1,34 +1,5 @@
 #include <iostream>
-using namespace std;
 
-void func(double arr[],int n)
-{
-  int maxInd=0;
-  int minInd=0;
-  for(int i=0;i<n;i++)
-    {
-      if(arr[i]>arr[maxInd])
-      {
-        maxInd=i;
-      }
-      else if (arr[i]<arr[minInd])
-      {
-        minInd=i;
-      }
-    }
-  arr[maxInd]=arr[minInd]=0;
-  
-}
 int main() {
-  const int n=16;
-  double arr[n];
-  for(int i=0;i<n;i++)
-    {
-      cin >> arr[i];
-    }
-  func(arr,n);
-  for(int i=0;i<n;i++)
-    {
-      cout<<arr[i]<<"\t";
-    }
+  std::cout << "Hello World!!!!!!!!!\n";
 }
